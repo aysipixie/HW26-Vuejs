23b3150bd9818bdc216dd3bfe54e089904f20e85
aysipixie <aysimaerdemirci@gmail.com>
2023-12-30 14:13:53 +0300

    first commit

---
+++ HW26-Vuejs/src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer>
+      <p>{{ footerText }}</p>
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        footerText: 'Vue Footer'
+      };
+    }
+  };
+  </script>
+  
+  <style>
+  /* Footer stilinizi buraya ekleyin */
+  </style>
+  
---
+++ HW26-Vuejs/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+      <h1>{{ title }}</h1>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        title: 'Vue Header'
+      };
+    }
+  };
+  </script>
+  
+  <style>
+  /* Header stilinizi buraya ekleyin */
+  </style>  
